f542a6c404f0f29934eca4c66f1255ac3381c1c1
piyushmchetu <piyushm@chetu.com>
2020-08-13 10:05:23 +0530

    hello there

---
+++ src/Component/main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Axios from 'axios';
+
+class Main extends Component {
+    constructor() {
+        super();
+        this.state = {name:['sibbu']}
+    }
+
+   
+    componentDidMount() {
+        Axios.get('http://localhost:5000/read').then(res => {
+            this.setState({name: res.data.name});           
+        });
+    }
+
+    render() {
+        let { name } = this.state;
+        let namelist = name.map(res => <p key={res}>{res}</p>)
+        return (
+            <>
+            <h1>list of name</h1>
+            {namelist}
+            </>
+        )
+    }
+}
+
+export default Main
